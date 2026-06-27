0a522da56968debb27f557fc5d4a6902f6071c12
unihelplb <115044263+unihelplb@users.noreply.github.com>
2026-06-27 14:45:10 +0300

    Delete js/config.example.js

--- js/config.example.js
+++
@@ -1,8 +0,0 @@
-/* config.example.js
-   Copy this file to config.js and paste your NPS API key.
-   Get a free key at https://www.nps.gov/subjects/developer/get-started.htm
-*/
-const CONFIG = {
-  NPS_KEY: "8LavRnquMzjJcQ7Aj2MLeYVMZ6ilISGTtwVqS0Fq",
-};
- 
